5c693d1b5d4df675cfda7eb6646fe1e1d122803d
leonardoct3 <leocarvalhoteixeira@gmail.com>
2025-01-14 17:35:20 -0300

    server.js

--- server.js
+++ server.js
@@ -8,3 +8,34 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const random = Math.floor(Math.random() * quotes.length);
+    res.send(quotes[random]);
+})
+
+app.get('/api/quotes', (req, res, next) => {
+    const person = req.query.person;
+    if (person) {
+        personQuotes = quotes.filter(quote => quote.person === person);
+        res.send({quotes: personQuotes});
+    } else {
+        res.send({quotes: quotes});
+    }
+})
+
+app.post('/api/quotes', (req, res, next) => {
+    const { person, quote } = req.query;
+    if (person && quote) {
+        newQuote = {
+            person, quote
+        }
+        quotes.push(newQuote);
+        res.status(201).send({quote: newQuote});
+    } else {
+        res.status(400).send();
+    }
+})
